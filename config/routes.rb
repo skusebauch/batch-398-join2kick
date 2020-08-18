74392a250cc19da4a661bda5be9d30d389d7a0bd
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-08-18 23:12:51 +0200

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :pitches, only: [:index, :show] do
-    resources :bookings, except: [:destroy]
+    resources :bookings, except: [:index, :destroy]
   end
   resources :bookings, only: [:index, :destroy]
 end
